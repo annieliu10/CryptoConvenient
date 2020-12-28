4c0ada0c6dda2046cb101b61bc37125805199895
annieliu10 <annieyt8880@gmail.com>
2020-12-28 13:28:56 +0800

    Security authenticatoin features implemented

--- reminders.py
+++ reminders.py
@@ -3,8 +3,24 @@ from flask import jsonify
 from flask import abort 
 from flask import make_response
 from flask import request
+from flask import url_for
+from flask_httpauth import HTTPBasicAuth
+
 
 reminder= Flask(__name__)
+## setting up an authentication
+
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'annie':
+        return "LYT20010102"
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 ## items with the same categ id belong to the same category 
@@ -37,12 +53,29 @@ reminders = [
 
 
 @reminder.route('/reminder/api/v1.0/reminders', methods = ['GET'])
+@auth.login_required
 def get_reminders():
-    return jsonify({'reminders': reminders})
+    tempList = []
+    for each in reminders:
+        tempList.append(make_public_reminder(each))  
+    return jsonify({'reminders': tempList})
+
+
+def make_public_reminder(reminder):
+    newreminder={}
+    for field in reminder:
+        if field == 'id':
+            newreminder['uri']= url_for('get_reminders', reminder_id=reminder['id'], _external=True)
+        else:
+            newreminder[field] = reminder[field]
+    return newreminder
+
+
 
 
 ## get reminders that have the same id//category 
 @reminder.route('/reminder/api/v1.0/reminders/<int:reminders_categ>', methods = ['GET'])
+@auth.login_required
 def get_specific_reminders(reminders_categ):
     ## only keep the tasks that have the associated id 
     specificreminders = filter(lambda reminder: reminder['categ'] == reminders_categ, reminders)
@@ -53,11 +86,12 @@ def get_specific_reminders(reminders_categ):
 
 
 @reminder.route('/reminder/api/v1.0/reminders/<int:reminders_id>', methods = ['GET'])
+@auth.login_required
 def get_specific_reminder(reminders_id):
-    specificreminder = {}
+
     for each_reminder in reminders:
         if(each_reminder['id']== reminders_id):
-            return jsonify({'specific reminder': specificreminder})
+            return jsonify({'specific reminder': each_reminder['id']})
 
     abort(404)
 
@@ -65,17 +99,18 @@ def get_specific_reminder(reminders_id):
 @reminder.route('/reminder/api/v1.0/reminders', methods=['POST'])
 def make_new_reminder():
     #request.json has the requested data 
-    if not request.json or not 'title' in request.json or not 'categ' in request.json:
+    if not request.json or not 'title' in request.json and not 'categ' in request.json:
         abort(400)
     reminder = {
         'categ': request.json['categ'],
         'id': reminders[-1]['id']+1,
         'title': request.json['title'],
+        ## second argument in the description: what you would replace it with if there isn't a description
         'description': request.json.get('description', ""),
         'completed': False
     }
     reminders.append(reminder)
-    return jsonify({'reminders': reminders}), 201
+    return jsonify({'reminder': reminder}), 201
 
 
 @reminder.route('/reminder/api/v1.0/reminders/<int:reminders_id>', methods = ['DELETE'])
@@ -103,14 +138,37 @@ def delete_categ(reminders_categ):
 
 
 @reminder.route('/reminder/api/v1.0/reminders/<int:reminders_id>', methods = ['PUT'])
+@auth.login_required
 def update_reminder(reminders_id):
-    
+    theone = {}
+    for each_ele in reminders:
+        if each_ele['id']== reminders_id:
+            theone = each_ele
+    if not request.json:
+        abort(400)
+        ## when "title" is in the json data but its value is not in string 
+    if 'title' in request.json and type(request.json['title']) is not str:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+    if 'completed' in request.json and type(request.json['completed']) is not bool:
+        abort (400)
+    if 'categ' in request.json and type(request.json['categ']) is not int:
+        abort (400)
 
 
+    ##change it, or default 
+    theone['title']=  request.json.get('title', theone['title'])
+    theone['description']= request.json.get('description', theone['description'])
+    theone['completed']= request.json.get('completed', theone['completed'])
+    theone['categ'] = request.json.get('categ', theone['completed'])
+    return jsonify({'reminder': theone})
 
 @reminder.errorhandler(404)
 def notfound(error):
     return make_response(jsonify({'error': "Not found oop"}), 404)
+
+
 
 
 
